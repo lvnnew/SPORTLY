39eb09b7364ff590125e5252f9f0e3029d4eaec2
Vladimir Leshenko <pirat_2-5@mail.ru>
2023-02-05 17:24:01 +0300

    add ym

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-img-element */
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
@@ -7,6 +8,26 @@ export default function Document() {
       <body>
         <Main />
         <NextScript />
+
+        <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+                m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+                (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+                ym(92333408, "init", {
+                      clickmap:true,
+                      trackLinks:true,
+                      accurateTrackBounce:true
+                });
+              `,
+            }}
+          />
+        <noscript>
+          <div>
+            <img src="https://mc.yandex.ru/watch/92333408" style={{ position:'absolute', left:'-9999px' }} alt="" />
+          </div>
+        </noscript>
       </body>
     </Html>
   );
